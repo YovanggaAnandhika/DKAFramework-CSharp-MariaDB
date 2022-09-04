e3b04007d70372b77812f0e3099e878ca5b59dae
Yovangga Anandhika <dhikaprivate@gmail.com>
2022-09-04 09:10:17 +0800

    add structure

--- MariaDB.cs
+++ MariaDB.cs
@@ -1,7 +1,5 @@
 ﻿namespace MariaDB
 {
-    public class MariaDB
-    {
+   
 
-    }
 }
---
+++ Modules/CRUD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MariaDB.Modules
+{
+    public class CRUD
+    {
+
+    }
+}
